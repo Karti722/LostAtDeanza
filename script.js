454a4c7a14f11b70dc558f9a14859162ed1d3960
Karti722 <98507922+Karti722@users.noreply.github.com>
2023-02-11 16:44:37 -0800

    Added the files of the website from my desktop.

---
+++ script.js
@@ -0,0 +1,12 @@
+function submission_confirmation() {
+    // TODO: Fill in the rest 
+    var name = document.getElementById("name").value;
+    var location = document.getElementById("location").value;
+    var incident = document.getElementById("incident").value;
+
+    console.log("Hi, Police HQ, There is a new lost report from the user.");
+    console.log(name + " has lost the item near the location: " + location);
+    console.log(incident);
+
+    alert("Your request has been sucessfully sent to the police station.");
+}
