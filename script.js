69eebcbdf09675ae2ea72afbe7a04fcb2fcff0f2
Kartikeya Kumaria <98507922+Karti722@users.noreply.github.com>
2023-02-17 15:47:12 -0800

    Added an alert for missing values
    If the user left either of the name, location, or incident values blank after the form was submitted, then an alert would now be displayed asking the user to fill in the missing values.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function submission_confirmation() {
-    // TODO: Fill in the rest 
+function submission_confirmation() { 
     var name = document.getElementById("name").value;
     var location = document.getElementById("location").value;
     var incident = document.getElementById("incident").value;
-
+//     If any items are left blank after submitting
+    if (name == "" || location == "" || incident == "" || ) {
+        alert("All items must be filled in");
+        return;
+    }
     console.log("Hi, Police HQ, There is a new lost report from the user.");
     console.log(name + " has lost the item near the location: " + location);
     console.log(incident);
-
     alert("Your request has been sucessfully sent to the police station.");
 }
