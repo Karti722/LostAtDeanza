cae876993edbfe0306c7b85d23943f1eaa8320ac
Kartikeya Kumaria <98507922+Karti722@users.noreply.github.com>
2023-02-17 16:00:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function submission_confirmation() {
     var name = document.getElementById("name").value;
     var location = document.getElementById("location").value;
     var incident = document.getElementById("incident").value;
+    //     If any items are left blank after submitting
+    if (name == "" || location == "" || incident == "") {
+        alert("All items must be filled in");
+        return;
+    }
     console.log("Hi, Police HQ, There is a new lost report from the user.");
     console.log(name + " has lost the item near the location: " + location);
     console.log(incident);
